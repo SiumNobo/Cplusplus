06a0721222abf6131659d0d95b87c4ba350c33c9
SiumNobo <siumnobo@gmail.com>
2023-07-28 10:05:38 +0600

    allpracprbusingconceptoop

--- Examprac.cpp
+++ Examprac.cpp
@@ -18,7 +18,7 @@ public:
     {
         return radius;
     }
-    void Area(float radius)
+    virtual void Area(float radius)
     {
         cout<<"Area"<<3.1416*radius*radius<<endl;
     }
@@ -26,10 +26,27 @@ public:
 };
 class circlethree:public circle
 {
+    string circlename3;
+public:
+circlethree(float radius,string circlename): circle(radius)
+    {
+          circlename3=circlename;
+    }
+    void area(float radius)
+    {
+        cout<<"Area"<<3.1416*radius*radius<<endl;
+    }
 
 };
 
 int main()
 {
+
+    circle c1=circle(3.16);
+    circle c2=circle(5);
+    circlethree c3=circlethree(7,"C3");
+
+    circle* cthree=&c3;
+    c3.Area(5);
 
 }
